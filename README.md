a7e6c59b985c0eec7a10e5e66bdc9c8e4e6612ad
Daniel Lorch <dlorch@gmail.com>
2019-07-14 22:53:17 +0200

    Implement 'showmount' with static answer

---
+++ README.md
@@ -0,0 +1,5 @@
+```
+$ sudo showmount -e 127.0.0.1
+Exports list on 127.0.0.1:
+/volume1/Public                     *
+```
--- main.go
+++ main.go
@@ -59,19 +59,11 @@ func handlePortmapRequest(requestBytes []byte, serverConn *net.UDPConn, clientAd
 		fmt.Println("Error decoding RPC message: ", err.Error())
 	}
 
-	fmt.Println(rpcMsg.XID)
-	fmt.Println(rpcMsg.MessageType)
-
 	err = binary.Read(requestBuffer, binary.BigEndian, &callBody)
 
 	if err != nil {
 		fmt.Println("Error decoding call body: ", err.Error())
 	}
-
-	fmt.Println(callBody.RPCVersion)
-	fmt.Println(callBody.Program)
-	fmt.Println(callBody.ProgramVersion)
-	fmt.Println(callBody.Procedure)
 
 	err = binary.Read(requestBuffer, binary.BigEndian, &credentials)
 
@@ -79,8 +71,12 @@ func handlePortmapRequest(requestBytes []byte, serverConn *net.UDPConn, clientAd
 		fmt.Println("Error decoding credentials: ", err.Error())
 	}
 
-	fmt.Println(credentials.Flavor)
-	fmt.Println(credentials.Length)
+	credentialsBody := make([]byte, credentials.Length)
+	err = binary.Read(requestBuffer, binary.BigEndian, &credentialsBody)
+
+	if err != nil {
+		fmt.Println("[mount] Error decoding credentials body: ", err.Error())
+	}
 
 	err = binary.Read(requestBuffer, binary.BigEndian, &verifier)
 
@@ -88,19 +84,18 @@ func handlePortmapRequest(requestBytes []byte, serverConn *net.UDPConn, clientAd
 		fmt.Println("Error decoding verifier: ", err.Error())
 	}
 
-	fmt.Println(verifier.Flavor)
-	fmt.Println(verifier.Length)
+	verifierBody := make([]byte, verifier.Length)
+	err = binary.Read(requestBuffer, binary.BigEndian, &verifierBody)
+
+	if err != nil {
+		fmt.Println("[mount] Error decoding verifier body: ", err.Error())
+	}
 
 	err = binary.Read(requestBuffer, binary.BigEndian, &mapping)
 
 	if err != nil {
-		fmt.Println("Error decoding verifier: ", err.Error())
+		fmt.Println("Error decoding mapping: ", err.Error())
 	}
-
-	fmt.Println(mapping.Program)
-	fmt.Println(mapping.Version)
-	fmt.Println(mapping.Protocol)
-	fmt.Println(mapping.Port)
 
 	/*
 	 * Response
@@ -141,22 +136,22 @@ func handlePortmapRequest(requestBytes []byte, serverConn *net.UDPConn, clientAd
 	serverConn.WriteToUDP(responseBuffer.Bytes(), clientAddr)
 }
 
-func main() {
+func runPortmapperService() {
 	serverAddr, err := net.ResolveUDPAddr("udp", ":111")
 
 	if err != nil {
-		fmt.Println("Error resolving UDP address: ", err.Error())
+		fmt.Println("[portmapper] Error resolving UDP address: ", err.Error())
 		os.Exit(1)
 	}
 
 	serverConn, err := net.ListenUDP("udp", serverAddr)
 
 	if err != nil {
-		fmt.Println("Error listening: ", err.Error())
+		fmt.Println("[portmap] Error listening: ", err.Error())
 		os.Exit(1)
 	}
 
-	fmt.Println("Listening at: ", serverAddr)
+	fmt.Println("[portmap] Listening at: ", serverAddr)
 
 	defer serverConn.Close()
 
@@ -166,10 +161,184 @@ func main() {
 		_, clientAddr, err := serverConn.ReadFromUDP(requestBytes)
 
 		if err != nil {
-			fmt.Println("Error receiving: ", err.Error())
+			fmt.Println("[portmap] Error receiving: ", err.Error())
 		} else {
-			fmt.Println("Received request from ", clientAddr)
+			fmt.Println("[portmap] Received request from ", clientAddr)
 			go handlePortmapRequest(requestBytes, serverConn, clientAddr)
 		}
 	}
+}
+
+func handleMountRequest(clientConnection net.Conn) {
+	requestBytes := make([]byte, 1024)
+
+	_, err := clientConnection.Read(requestBytes)
+
+	if err != nil {
+		fmt.Println("[mount] Error reading: ", err.Error())
+	}
+
+	/*
+	 * Request
+	 */
+	var requestBuffer = bytes.NewBuffer(requestBytes)
+	var fragmentHeader uint32
+	var rpcMsg rpcv2.RPCMsg
+	var callBody rpcv2.CallBody
+	var credentials rpcv2.OpaqueAuth
+	var verifier rpcv2.OpaqueAuth
+
+	err = binary.Read(requestBuffer, binary.BigEndian, &fragmentHeader)
+
+	if err != nil {
+		fmt.Println("[mount] Error reading fragment header: ", err.Error())
+	}
+
+	err = binary.Read(requestBuffer, binary.BigEndian, &rpcMsg)
+
+	if err != nil {
+		fmt.Println("[mount] Error decoding RPC message: ", err.Error())
+	}
+
+	err = binary.Read(requestBuffer, binary.BigEndian, &callBody)
+
+	if err != nil {
+		fmt.Println("[mount] Error decoding call body: ", err.Error())
+	}
+
+	err = binary.Read(requestBuffer, binary.BigEndian, &credentials)
+
+	if err != nil {
+		fmt.Println("[mount] Error decoding credentials: ", err.Error())
+	}
+
+	credentialsBody := make([]byte, credentials.Length)
+	err = binary.Read(requestBuffer, binary.BigEndian, &credentialsBody)
+
+	if err != nil {
+		fmt.Println("[mount] Error decoding credentials body: ", err.Error())
+	}
+
+	err = binary.Read(requestBuffer, binary.BigEndian, &verifier)
+
+	if err != nil {
+		fmt.Println("[mount] Error decoding verifier: ", err.Error())
+	}
+
+	verifierBody := make([]byte, verifier.Length)
+	err = binary.Read(requestBuffer, binary.BigEndian, &verifierBody)
+
+	if err != nil {
+		fmt.Println("[mount] Error decoding verifier body: ", err.Error())
+	}
+
+	/*
+	 * Response
+	 */
+	var responseBuffer = new(bytes.Buffer)
+
+	rpcResponse := rpcv2.RPCMsg{
+		XID:         rpcMsg.XID,
+		MessageType: rpcv2.Reply,
+	}
+
+	err = binary.Write(responseBuffer, binary.BigEndian, &rpcResponse)
+
+	replyBody := rpcv2.ReplyBody{
+		ReplyStatus: rpcv2.MessageAccepted,
+	}
+
+	err = binary.Write(responseBuffer, binary.BigEndian, &replyBody)
+
+	verifierReply := rpcv2.OpaqueAuth{
+		Flavor: rpcv2.AuthenticationNull,
+		Length: 0,
+	}
+
+	successReply := rpcv2.AcceptedReplySuccess{
+		Verifier:    verifierReply,
+		AcceptState: rpcv2.Success,
+	}
+
+	err = binary.Write(responseBuffer, binary.BigEndian, &successReply)
+
+	// --- mount service body
+
+	var valueFollowsYes uint32
+	valueFollowsYes = 1
+
+	var valueFollowsNo uint32
+	valueFollowsNo = 0
+
+	directoryContents := "/volume1/Public"
+	directoryLength := uint32(len(directoryContents))
+
+	groupContents := "*"
+	groupLength := uint32(len(groupContents))
+
+	fillBytes := uint8(0)
+
+	err = binary.Write(responseBuffer, binary.BigEndian, &valueFollowsYes)
+	err = binary.Write(responseBuffer, binary.BigEndian, &directoryLength)
+	_, err = responseBuffer.Write([]byte(directoryContents))
+	err = binary.Write(responseBuffer, binary.BigEndian, &fillBytes)
+
+	err = binary.Write(responseBuffer, binary.BigEndian, &valueFollowsYes)
+	err = binary.Write(responseBuffer, binary.BigEndian, &groupLength)
+	_, err = responseBuffer.Write([]byte(groupContents))
+	err = binary.Write(responseBuffer, binary.BigEndian, &fillBytes)
+	err = binary.Write(responseBuffer, binary.BigEndian, &fillBytes)
+	err = binary.Write(responseBuffer, binary.BigEndian, &fillBytes)
+	err = binary.Write(responseBuffer, binary.BigEndian, &valueFollowsNo)
+
+	err = binary.Write(responseBuffer, binary.BigEndian, &valueFollowsNo)
+
+	// ---- fragments
+
+	var fragmentBuffer = new(bytes.Buffer)
+	lastFragment := uint32(1 << 31)
+	fragmentLength := uint32(responseBuffer.Len())
+
+	err = binary.Write(fragmentBuffer, binary.BigEndian, lastFragment|fragmentLength)
+
+	// ---- end
+
+	fragmentBuffer.Write(responseBuffer.Bytes())
+
+	_, err = clientConnection.Write(fragmentBuffer.Bytes())
+
+	if err != nil {
+		fmt.Println("[mount] Error sending response: ", err.Error())
+	}
+
+	clientConnection.Close()
+}
+
+func runMountService() {
+	serverConn, err := net.Listen("tcp", ":892")
+
+	if err != nil {
+		fmt.Println("[mount] Error listening: ", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println("[mount] Listening at: ", serverConn.Addr())
+
+	defer serverConn.Close()
+
+	for {
+		clientConnection, err := serverConn.Accept()
+
+		if err != nil {
+			fmt.Println("[mount] Error receiving: ", err.Error())
+		} else {
+			fmt.Println("[mount] Received request from ", clientConnection.RemoteAddr())
+			go handleMountRequest(clientConnection)
+		}
+	}
+}
+
+func main() {
+	go runPortmapperService()
+	runMountService()
 }
