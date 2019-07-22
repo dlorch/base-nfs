009861cfb85914fdfda7f45bf20f9e80cf360af6
Daniel Lorch <dlorch@gmail.com>
2019-07-22 21:00:08 +0200

    Implement nfsProcedure3FSInfo

--- nfsv3/protocol.go
+++ nfsv3/protocol.go
@@ -31,10 +31,45 @@
 package nfsv3
 
 import (
+	"bytes"
+	"encoding/binary"
 	"fmt"
 
 	"github.com/dlorch/nfsv3/rpcv2"
 )
+
+// FSInfoArgs (struct FSINFOargs)
+type FSInfoArgs struct {
+	FileHandle []byte
+}
+
+// FSInfo3ResultOK (struct FSINFO3resok)
+type FSInfo3ResultOK struct {
+	FSInfo3Result
+	objattributes        uint32 // TODO
+	rtmax                uint32
+	rtpref               uint32
+	rtmult               uint32
+	wtmax                uint32
+	wtpref               uint32
+	wtmult               uint32
+	dtpref               uint32
+	maxfilesize          uint64
+	timedeltaseconds     uint32
+	timedeltananoseconds uint32
+	properties           uint32
+}
+
+// FSInfo3ResultFail (struct FSINFO3resfail)
+type FSInfo3ResultFail struct {
+	FSInfo3Result
+	// TODO post_op_attr obj_attributes
+}
+
+// FSInfo3Result (union FSINFO3res)
+type FSInfo3Result struct {
+	status uint32
+}
 
 // Constants for mount protocol (RFC1813)
 const (
@@ -62,6 +97,7 @@ const (
 	NFSProcedure3FSInfo        uint32 = 19     // NFSPROC3_FSINFO
 	NFSProcedure3PathConf      uint32 = 20     // NFSPROC3_PATHCONF
 	NFSProcedure3Commint       uint32 = 21     // NFSPROC3_COMMIT
+	NFS3OK                     uint32 = 0      // NFS3_OK
 )
 
 func nfsProcedure3Null(request *rpcv2.RPCRequest) *rpcv2.RPCResponse {
@@ -113,8 +149,79 @@ func nfsProcedure3Access(request *rpcv2.RPCRequest) *rpcv2.RPCResponse {
 }
 
 func nfsProcedure3FSInfo(request *rpcv2.RPCRequest) *rpcv2.RPCResponse {
-	fmt.Println("nfsProcedure3FSInfo")
-	return nil
+	// parse request
+	requestBuffer := bytes.NewBuffer(request.RequestBody)
+
+	var fileHandleLength uint32
+
+	err := binary.Read(requestBuffer, binary.BigEndian, &fileHandleLength)
+
+	if err != nil {
+		fmt.Println("Error: ", err.Error())
+		// TODO
+	}
+
+	fsInfoArgs := FSInfoArgs{
+		FileHandle: make([]byte, fileHandleLength), // TODO unsafe?
+	}
+
+	err = binary.Read(requestBuffer, binary.BigEndian, &fsInfoArgs.FileHandle)
+
+	if err != nil {
+		fmt.Println("Error: ", err.Error())
+		// TODO
+	}
+
+	// prepare result
+	fsInfoResult := FSInfo3ResultOK{
+		FSInfo3Result: FSInfo3Result{
+			status: NFS3OK,
+		},
+		objattributes:        0,
+		rtmax:                131072,
+		rtpref:               131072,
+		rtmult:               4096,
+		wtmax:                131072,
+		wtpref:               131072,
+		wtmult:               4096,
+		dtpref:               4096,
+		maxfilesize:          8796093022207,
+		timedeltaseconds:     1,
+		timedeltananoseconds: 0,
+		properties:           0x0000001b,
+	}
+
+	rpcResponse := &rpcv2.RPCResponse{
+		RPCMessage: rpcv2.RPCMsg{
+			XID:         request.RPCMessage.XID,
+			MessageType: rpcv2.Reply,
+		},
+		ReplyBody: rpcv2.ReplyBody{
+			ReplyStatus: rpcv2.MessageAccepted,
+		},
+		AcceptedReply: rpcv2.AcceptedReply{
+			Verifier: rpcv2.OpaqueAuth{
+				Flavor: rpcv2.AuthenticationNull,
+				Length: 0,
+			},
+			AcceptState: rpcv2.Success,
+		},
+	}
+
+	// create response
+	var resultBuffer = new(bytes.Buffer)
+
+	err = binary.Write(resultBuffer, binary.BigEndian, &fsInfoResult)
+
+	if err != nil {
+		fmt.Println("Error: ", err.Error())
+		// TODO
+	}
+
+	rpcResponse.AcceptedReply.Results = make([]byte, resultBuffer.Len())
+	copy(rpcResponse.AcceptedReply.Results, resultBuffer.Bytes())
+
+	return rpcResponse
 }
 
 func nfsProcedure3PathConf(request *rpcv2.RPCRequest) *rpcv2.RPCResponse {
