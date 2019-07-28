8499f8c06076bc637a5a59eb50798e7bfc16b91d
Daniel Lorch <dlorch@gmail.com>
2019-07-28 20:57:04 +0200

    Add more tests

--- xdr/xdr_test.go
+++ xdr/xdr_test.go
@@ -28,3 +28,73 @@ func TestEncodeSimple(t *testing.T) {
 		t.Fatalf("Expected %v but got %v", simpleExpect, got)
 	}
 }
+
+type Nested struct {
+	Value uint32
+	Inner
+}
+
+type Inner struct {
+	Value uint32
+}
+
+var nested = &Nested{
+	Value: 12,
+	Inner: Inner{
+		Value: 13,
+	},
+}
+
+var nestedExpect = []byte{0, 0, 0, 12, 0, 0, 0, 13}
+
+func TestEncodeNested(t *testing.T) {
+	got, err := xdr.Marshal(nested)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if !reflect.DeepEqual(got, nestedExpect) {
+		t.Fatalf("Expected %v but got %v", nestedExpect, got)
+	}
+}
+
+type FixedByteArray struct {
+	Data [4]byte
+}
+
+var fixedByteArray = &FixedByteArray{
+	Data: [4]byte{55, 43, 99, 102},
+}
+
+var fixedByteArrayExpect = []byte{55, 43, 99, 102}
+
+func TestEncodeFixedByteArray(t *testing.T) {
+	got, err := xdr.Marshal(fixedByteArray)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if !reflect.DeepEqual(got, fixedByteArrayExpect) {
+		t.Fatalf("Expected %v but got %v", fixedByteArrayExpect, got)
+	}
+}
+
+type DynamicallySizedValues struct {
+	Data []byte
+	Name string
+}
+
+var dynamicallySizedValues = &DynamicallySizedValues{
+	Data: []byte{41, 22, 13, 4, 15}, // encodes as: length + bytes
+	Name: "Gopher",                  // encodes as: length + bytes + padding (total length must be multiple of four)
+}
+
+var dynamicallySizedValuesExpect = []byte{5, 41, 22, 13, 4, 15, 6, 0, 0, 0, 6, 71, 111, 112, 104, 101, 114, 0, 0}
+
+func TestEncodeDynamicallySizedValues(t *testing.T) {
+	got, err := xdr.Marshal(dynamicallySizedValues)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if !reflect.DeepEqual(got, dynamicallySizedValuesExpect) {
+		t.Fatalf("Expected %v but got %v", dynamicallySizedValuesExpect, got)
+	}
+}
