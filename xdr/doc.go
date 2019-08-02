9e1738f083a56edf439a2035d46c08a8cb642465
Daniel Lorch <dlorch@gmail.com>
2019-08-02 13:47:53 +0200

    Remove README.md in favor of package-level Go documentation

---
+++ xdr/doc.go
@@ -0,0 +1,9 @@
+// Copyright 2019 Daniel Lorch. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package xdr facilitates encoding and decoding of XDR (External Data Representation
+Standard) formats as defined in https://tools.ietf.org/html/rfc1014
+*/
+package xdr
