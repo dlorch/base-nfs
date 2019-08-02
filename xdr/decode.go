0e74f82431a92d62f243252d805a1c815658a735
Daniel Lorch <dlorch@gmail.com>
2019-08-02 13:56:04 +0200

    Add skeleton decode.go

---
+++ xdr/decode.go
@@ -0,0 +1,10 @@
+// Copyright 2019 Daniel Lorch. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package xdr
+
+// Unmarshal deserializes a byte array to an XDR format
+func Unmarshal(data []byte, v interface{}) error {
+	return nil // TODO
+}
