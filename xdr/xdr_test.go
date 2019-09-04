e66a3c8e00a88b5e6e5661a0f0cddad486ecd0d3
Daniel Lorch <dlorch@gmail.com>
2019-09-04 21:35:49 +0200

    Fix test

--- xdr/xdr_test.go
+++ xdr/xdr_test.go
@@ -205,7 +205,6 @@ var invalidSizedValuesBytes = []byte{0, 0, 0, 3, 1, 2}
 func TestInvalidSizedValues(t *testing.T) {
 	got := &DynamicallySizedValues{}
 	_, err := xdr.Unmarshal(invalidSizedValuesBytes, got)
-	t.Fatal(err)
 	if err == nil {
 		t.Fatalf("Expected error, but got %v", got)
 	}
