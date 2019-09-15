7808ad4de18a2d1b98cd6fcc5254af30cebd068e
Daniel Lorch <dlorch@gmail.com>
2019-09-15 20:54:52 +0200

    Better group constants

--- nfsv3/protocol.go
+++ nfsv3/protocol.go
@@ -48,32 +48,44 @@ type PostOperationFileHandle3 struct {
 	Handle        []byte // TODO struct nfs_fh3
 }
 
-// Constants for mount protocol (RFC1813)
+// RPC Constants for NFS3 Protocol
 const (
-	Program                    uint32 = 100003 // Mount service program number
-	Version                    uint32 = 3      // Mount service program version
-	NFSProcedure3Null          uint32 = 0      // NFSPROC3_NULL
-	NFSProcedure3GetAttributes uint32 = 1      // NFSPROC3_GETATTR
-	NFSProcedure3SetAttributes uint32 = 2      // NFSPROC3_SETATTR
-	NFSProcedure3Lookup        uint32 = 3      // NFSPROC3_LOOKUP
-	NFSProcedure3Access        uint32 = 4      // NFSPROC3_ACCESS
-	NFSProcedure3Readlink      uint32 = 5      // NFSPROC3_READLINK
-	NFSProcedure3Read          uint32 = 6      // NFSPROC3_READ
-	NFSProcedure3Write         uint32 = 7      // NFSPROC3_WRITE
-	NFSProcedure3Create        uint32 = 8      // NFSPROC3_CREATE
-	NFSProcedure3MkDir         uint32 = 9      // NFSPROC3_MKDIR
-	NFSProcedure3Symlink       uint32 = 10     // NFSPROC3_SYMLINK
-	NFSProcedure3MkNod         uint32 = 11     // NFSPROC3_MKNOD
-	NFSProcedure3Remove        uint32 = 12     // NFSPROC3_REMOVE
-	NFSProcedure3RmDir         uint32 = 13     // NFSPROC3_RMDIR
-	NFSProcedure3Rename        uint32 = 14     // NFSPROC3_RENAME
-	NFSProcedure3Link          uint32 = 15     // NFSPROC3_LINK
-	NFSProcedure3ReadDir       uint32 = 16     // NFSPROC3_READDIR
-	NFSProcedure3ReadDirPlus   uint32 = 17     // NFSPROC3_READDIRPLUS
-	NFSProcedure3FSStat        uint32 = 18     // NFSPROC3_FSSTAT
-	NFSProcedure3FSInfo        uint32 = 19     // NFSPROC3_FSINFO
-	NFSProcedure3PathConf      uint32 = 20     // NFSPROC3_PATHCONF
-	NFSProcedure3Commint       uint32 = 21     // NFSPROC3_COMMIT
-	NFS3OK                     uint32 = 0      // NFS3_OK
-	NFS3CookieVerifierSize     uint32 = 8      // The size in bytes of the opaque cookie verifier passed by READDIR and READDIRPLUS (NFS3_COOKIEVERFSIZE)
+	Program uint32 = 100003 // Mount service program number
+	Version uint32 = 3      // Mount service program version
+)
+
+// Sizes, given in decimal bytes, of various XDR structures
+const (
+	NFS3CookieVerifierSize uint32 = 8 // The size in bytes of the opaque cookie verifier passed by READDIR and READDIRPLUS (NFS3_COOKIEVERFSIZE)
+)
+
+// Returned with every procedure's results except for the NULL procedure (enum nfsstat3)
+const (
+	NFS3OK uint32 = 0 // NFS3_OK
+)
+
+// RPC procedure numbers
+const (
+	NFSProcedure3Null          uint32 = 0  // NFSPROC3_NULL
+	NFSProcedure3GetAttributes uint32 = 1  // NFSPROC3_GETATTR
+	NFSProcedure3SetAttributes uint32 = 2  // NFSPROC3_SETATTR
+	NFSProcedure3Lookup        uint32 = 3  // NFSPROC3_LOOKUP
+	NFSProcedure3Access        uint32 = 4  // NFSPROC3_ACCESS
+	NFSProcedure3Readlink      uint32 = 5  // NFSPROC3_READLINK
+	NFSProcedure3Read          uint32 = 6  // NFSPROC3_READ
+	NFSProcedure3Write         uint32 = 7  // NFSPROC3_WRITE
+	NFSProcedure3Create        uint32 = 8  // NFSPROC3_CREATE
+	NFSProcedure3MkDir         uint32 = 9  // NFSPROC3_MKDIR
+	NFSProcedure3Symlink       uint32 = 10 // NFSPROC3_SYMLINK
+	NFSProcedure3MkNod         uint32 = 11 // NFSPROC3_MKNOD
+	NFSProcedure3Remove        uint32 = 12 // NFSPROC3_REMOVE
+	NFSProcedure3RmDir         uint32 = 13 // NFSPROC3_RMDIR
+	NFSProcedure3Rename        uint32 = 14 // NFSPROC3_RENAME
+	NFSProcedure3Link          uint32 = 15 // NFSPROC3_LINK
+	NFSProcedure3ReadDir       uint32 = 16 // NFSPROC3_READDIR
+	NFSProcedure3ReadDirPlus   uint32 = 17 // NFSPROC3_READDIRPLUS
+	NFSProcedure3FSStat        uint32 = 18 // NFSPROC3_FSSTAT
+	NFSProcedure3FSInfo        uint32 = 19 // NFSPROC3_FSINFO
+	NFSProcedure3PathConf      uint32 = 20 // NFSPROC3_PATHCONF
+	NFSProcedure3Commint       uint32 = 21 // NFSPROC3_COMMIT
 )
